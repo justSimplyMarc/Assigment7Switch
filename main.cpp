1bccd37a845803a6fcb86b9cd08d81202ddf9417
justSimplyMarc <marcjuliussorianogabilo@gmail.com>
2024-10-15 22:22:08 +0800

    Final

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main() {
     while(choice == 'y'){
         std::cout << "DAY OF THE WEEK" << std::endl;
         std::cout << "1 - Monday" << std::endl;
-        std::cout << "2 - Teusday" << std::endl;
+        std::cout << "2 - Tuesday" << std::endl;
         std::cout << "3 - Wednesday" << std::endl;
         std::cout << "4 - Thursday" << std::endl;
         std::cout << "5 - Friday" << std::endl;
-        std::cout << "6 - Staturday" << std::endl;
+        std::cout << "6 - Saturday" << std::endl;
         std::cout << "7 - Sunday" << std::endl;
         std::cout << "" << std::endl;
  
@@ -25,22 +25,22 @@ int main() {
                 std::cout << "You selected Monday" << std::endl;
                 break;        
             case 2:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Tuesday" << std::endl;
                 break;
             case 3:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Wednesday" << std::endl;
                 break;
             case 4:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Thursday" << std::endl;
                 break;
             case 5:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Friday" << std::endl;
                 break;
             case 6:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Saturday" << std::endl;
                 break;
             case 7:
-                std::cout << "You selected Monday" << std::endl;
+                std::cout << "You selected Sunday" << std::endl;
                 break;
             default:
                 std::cout << "Invalid input!" << std::endl;
