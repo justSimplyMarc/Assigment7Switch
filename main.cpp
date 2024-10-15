4971edbfec90bec41e3239b1afcfae791c9c9b70
justSimplyMarc <marcjuliussorianogabilo@gmail.com>
2024-10-15 22:39:45 +0800

    FInal Final

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ int main() {
     char choice;
  
     choice = 'y';
-    while(choice == 'y'){
+    while(choice == 'y'||choice == 'Y' ){
         std::cout << "DAY OF THE WEEK" << std::endl;
         std::cout << "1 - Monday" << std::endl;
-        std::cout << "2 - Tuesday" << std::endl;
+        std::cout << "2 - Teusday" << std::endl;
         std::cout << "3 - Wednesday" << std::endl;
         std::cout << "4 - Thursday" << std::endl;
         std::cout << "5 - Friday" << std::endl;
-        std::cout << "6 - Saturday" << std::endl;
+        std::cout << "6 - Staturday" << std::endl;
         std::cout << "7 - Sunday" << std::endl;
         std::cout << "" << std::endl;
  
@@ -25,7 +25,7 @@ int main() {
                 std::cout << "You selected Monday" << std::endl;
                 break;        
             case 2:
-                std::cout << "You selected Tuesday" << std::endl;
+                std::cout << "You selected Teusday" << std::endl;
                 break;
             case 3:
                 std::cout << "You selected Wednesday" << std::endl;
@@ -37,7 +37,7 @@ int main() {
                 std::cout << "You selected Friday" << std::endl;
                 break;
             case 6:
-                std::cout << "You selected Saturday" << std::endl;
+                std::cout << "You selected Staturday" << std::endl;
                 break;
             case 7:
                 std::cout << "You selected Sunday" << std::endl;
@@ -47,8 +47,22 @@ int main() {
                 break;
         }
  
-        std::cout << "Do you want to try again? (y/n): ";
+       
+        invalidInput:
+        std::cout << "Do you want to try again? (y/n): ";        
         std::cin >> choice;
+ 
+        switch(choice){
+            case 'y':
+            case 'Y':  
+                break;                                        
+            case 'n':
+            case 'N':            
+                break;
+            default:
+                std::cout << "Invalid input!" << std::endl;
+                goto invalidInput;
+        }
     }
  
     std::cout << "Terminating application...";
